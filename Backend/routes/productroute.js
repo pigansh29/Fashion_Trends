01fec0cd59a1186e0b9a5c560c267879310e4a69
Pigansh Gaur <gaurpigansh@gmail.com>
2024-08-08 13:13:16 +0530

    updated commit

--- Backend/routes/productroute.js
+++ Backend/routes/productroute.js
@@ -1,13 +1,12 @@
+const express = require("express");
+const getproductcontroller = require("../controllers/products/getproductcontroller");
+const router = express.Router();
 
-const express=require("express")
-const getproductcontroller=require("../controllers/products/getproductcontroller")
-const router=express.Router()
-
-router.get('/',getproductcontroller)
-router.get('/category/:category',getproductcontroller)
-router.get('/name/:name',getproductcontroller)
-router.get('/id/:id',getproductcontroller)
-router.get('/random',getproductcontroller)
-router.get('/top-rated',getproductcontroller)
-router.get('/subcategory/:subcategory',getproductcontroller)
-module.exports=router
+router.get("/", getproductcontroller);
+router.get("/category/:category", getproductcontroller);
+router.get("/name/:name", getproductcontroller);
+router.get("/id/:id", getproductcontroller);
+router.get("/random", getproductcontroller);
+router.get("/top-rated", getproductcontroller);
+router.get("/subcategory/:subcategory", getproductcontroller);
+module.exports = router;
